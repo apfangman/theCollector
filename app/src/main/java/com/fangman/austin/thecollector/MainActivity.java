3f55ac61fdb8b30b08f38602398436260a0a1fef
apfangman <austinfangman@yahoo.com>
2017-04-14 23:52:38 -0500

    Added class to get information from an API

--- app/src/main/java/com/fangman/austin/thecollector/MainActivity.java
+++ app/src/main/java/com/fangman/austin/thecollector/MainActivity.java
@@ -1,15 +1,29 @@
 package com.fangman.austin.thecollector;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends ActionBarActivity {
+
+    ProgressBar progressBar;
+    String str;
+
+    //TODO: Get user id. Should be passed to this activity after login.
+    static final String API_URL = "http://104.236.238.213/<<UserId>>";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +65,50 @@ public class MainActivity extends ActionBarActivity {
 
     private void goToCollections()
     {
+        new Retriever().execute();
         Intent intent = new Intent(this, CollectionsActivity.class);
         startActivity(intent);
+    }
+
+    class Retriever extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        protected String doInBackground(Void... urls) {
+            try {
+                URL url = new URL(API_URL);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            progressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+            str = response;
+        }
     }
 }
