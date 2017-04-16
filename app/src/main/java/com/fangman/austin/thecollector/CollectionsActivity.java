18010907e9a6509fecc8a499ff28d6cfb66078aa
apfangman <austinfangman@yahoo.com>
2017-04-15 19:03:45 -0500

    Moved collections request to collections page

--- app/src/main/java/com/fangman/austin/thecollector/CollectionsActivity.java
+++ app/src/main/java/com/fangman/austin/thecollector/CollectionsActivity.java
@@ -1,15 +1,34 @@
 package com.fangman.austin.thecollector;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class CollectionsActivity extends ActionBarActivity {
 
+    ProgressBar progressBar;
+
+    //TODO: Get user id. Should be passed to this activity after login.
+    static final String API_URL = "http://104.236.238.213/api/getCollections/<<UserId>>";
+
+    CollectionData cd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        new Retriever().execute();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collections);
     }
@@ -35,5 +54,61 @@ public class CollectionsActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class Retriever extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        protected String doInBackground(Void... urls) {
+            try {
+                URL url = new URL(API_URL);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            progressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+            cd = new Gson().fromJson(response, CollectionData.class);
+        }
+    }
+
+    class CollectionData
+    {
+        private Short id;
+        private String name;
+        private String picture;
+        private Short userId;
+        private Short collectionId;
+
+        public String getName() { return name; }
+        public String getPicture() { return picture; }
+        public Short getUserId() { return userId; }
+        public Short getCollectionId() { return collectionId; }
     }
 }
