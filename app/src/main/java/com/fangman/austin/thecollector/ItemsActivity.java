eff06106f7c1c7c9d22e33117958a18320be489a
apfangman <austinfangman@yahoo.com>
2017-04-21 22:32:07 -0500

    Added functionality for items activity

--- app/src/main/java/com/fangman/austin/thecollector/ItemsActivity.java
+++ app/src/main/java/com/fangman/austin/thecollector/ItemsActivity.java
@@ -1,19 +1,52 @@
 package com.fangman.austin.thecollector;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 
 
 public class ItemsActivity extends ActionBarActivity {
+
+    ProgressBar progressBar;
+
+    static String API_URL = "http://104.236.238.213/api/getItemsForCollectionForUser/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+
+        Intent intent = getIntent();
+        progressBar = (ProgressBar) findViewById(R.id.itemsProgressBar);
+        TextView textView = (TextView)findViewById(R.id.itemHeading);
+        textView.setText(intent.getStringExtra("collectionName"));
+        API_URL = API_URL + intent.getIntExtra("collectionId", -1);
+        //new Retriever().execute();
     }
 
+    private void setItemList(List<ItemData> dataList)
+    {
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +68,67 @@ public class ItemsActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class Retriever extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        protected String doInBackground(Void... urls) {
+            try {
+                URL url = new URL(API_URL);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            progressBar.setVisibility(View.GONE);
+            Log.i("INFO", response);
+            List<ItemData> dataList = new Gson().fromJson(response, new TypeToken<List<ItemData>>(){}.getType());
+            setItemList(dataList);
+        }
+    }
+
+    class ItemData
+    {
+        private Short id;
+        private String name;
+        private String picture;
+        private Short userId;
+        private Short collectionId;
+
+        public String getName() { return name; }
+        public String getPicture() { return picture; }
+        public Short getUserId() { return userId; }
+        public Short getCollectionId() { return collectionId; }
+
+        public String toString()
+        {
+            return name;
+        }
     }
 }
